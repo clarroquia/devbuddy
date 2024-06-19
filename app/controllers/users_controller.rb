79e42dab0b02aded0eeae125f90acc6458be19d8
David Wenk <david@thedavidwenk.com>
2024-06-19 15:21:49 +0200

    Revert "User profile"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,2 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
-  before_action :set_user, only: [:show]
-
-  def show
-    # @user is set by the set_user method
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:alert] = "User not found."
-    redirect_to root_path
-  end
 end
