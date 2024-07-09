52f09c74caa19a48b595fac4b0504ed969fa93aa
Robert Baufeld <rbaufeld@posteo.de>
2024-07-09 08:53:41 +0200

    added dependent destroy to time_slot model

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,6 +1,6 @@
 class TimeSlot < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :start_time, presence: true
   validates :end_time, presence: true
